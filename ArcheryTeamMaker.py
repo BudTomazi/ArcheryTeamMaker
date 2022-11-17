25eae6a9b9f23d3869e46462a59bc29dfc087441
BudTomazi <56653265+BudTomazi@users.noreply.github.com>
2022-11-16 17:06:16 -0800

    Update ArcheryTeamMaker.py

--- ArcheryTeamMaker.py
+++ ArcheryTeamMaker.py
@@ -2,41 +2,48 @@ import numpy as np
 import random as r
 import pandas as pd
 
-
-
 def correctness(scores):
-	a = np.array(scores)
-	b = a.shape[0]
-	a = np.reshape(a, (int(b/3), 3))
-	a = np.sum(a, axis=1)
-	a = np.std(a)
-	return a
+	averaged_scores = average_score(scores)
+	variance = np.std(averaged_scores)
+	return variance
 
-# df = pd.read_csv('TestScores.csv')
+def average_score(scores):
+	score_array = np.array(scores)
+	size = score_array.shape[0]
+	teams_array = np.reshape(score_array, (int(size/3), 3))
+	team_scores = np.sum(teams_array, axis=1)
+	team_sizes = np.sum(np.where(teams_array > 0, 1, 0), axis=1)
+	averaged_scores = team_scores / team_sizes
+	return averaged_scores
+
 df = pd.read_csv('GBOScores.csv')
 
-
 scores = df['Score'].tolist()
-# scores = [np.random.randint(0,301) for i in range(30)]
 names = df['Name'].tolist()
-
+bad_values = []
 l = len(scores)
 
-if l%3==2:
-	names.insert(l-2, "None")
-	names.append("None")
-	scores.insert(l-2, 0)
-	scores.append(0)
-elif l%3==1:
-	scores.append(0)
-	names.append("None")
+if l%3==1:
+  names.insert(l-2, "None")
+  names.append("None")
+  scores.insert(l-2, 0)
+  scores.append(0)
+  bad_values = [l+1, l-2]
+elif l%3==2:
+  scores.append(0)
+  names.append("None")
+  bad_values = [l]
+
+
+length = len(scores)
+teams = int(length/3)
 
 j=5
 curr_score = correctness(scores)
 for i in range(100):
 	for a in range(l):
 		for b in range(l):
-			if a == b:
+			if a == b or a in bad_values or b in bad_values:
 				continue
 
 			new_scores = scores[:]
@@ -51,6 +58,9 @@ for i in range(100):
 
 
 print(curr_score)
-print(np.reshape(np.array(names), (l/3,3)))
-print(np.sum(np.reshape(np.array(scores), (l/3,3)), axis=1))
 
+output_teams = np.reshape(np.array(names), (teams,3))
+output_scores = average_score(scores)
+
+for i in range(teams):
+  print("Team:", output_teams[i], "Average Score Score:", output_scores[i])
